278c95f40ec9b6ecb43b36e6175218b3c7fc2599
Koivunen Sara <a1300842@myy.haaga-helia.fi>
2018-02-02 12:36:45 +0200

    First commit

---
+++ Kirjakauppaprojekti/src/main/java/fi/hh/swd20/Kirjakauppaprojekti/KirjakauppaprojektiApplication.java
@@ -0,0 +1,12 @@
+package fi.hh.swd20.Kirjakauppaprojekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KirjakauppaprojektiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KirjakauppaprojektiApplication.class, args);
+	}
+}
