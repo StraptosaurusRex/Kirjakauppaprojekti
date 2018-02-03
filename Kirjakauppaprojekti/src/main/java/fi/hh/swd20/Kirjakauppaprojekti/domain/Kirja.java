a0bd6ef84af59acdd61066dca73b165e97df70d5
Your Name <you@example.com>
2018-02-03 15:25:29 +0200

    second commit

--- Kirjakauppaprojekti/src/main/java/fi/hh/swd20/Kirjakauppaprojekti/domain/Kirja.java
+++ Kirjakauppaprojekti/src/main/java/fi/hh/swd20/Kirjakauppaprojekti/domain/Kirja.java
@@ -2,4 +2,72 @@ package fi.hh.swd20.Kirjakauppaprojekti.domain;
 
 public class Kirja {
 
+	private String nimi;
+	private String tekija;
+	private String vuosi;
+	private String isbn;
+	private String hinta;
+	
+	public Kirja() {
+		this.nimi = null;
+		this.tekija = null;
+		this.vuosi = null;
+		this.isbn = null;
+		this.hinta = null;
+	}
+	
+	public Kirja(String nimi, String tekija, String vuosi, String isbn, String hinta) {
+		this.nimi = nimi;
+		this.tekija = tekija;
+		this.vuosi = vuosi;
+		this.isbn = isbn;
+		this.hinta = hinta;
+	}
+
+	public String getNimi() {
+		return nimi;
+	}
+
+	public void setNimi(String nimi) {
+		this.nimi = nimi;
+	}
+
+	public String getTekija() {
+		return tekija;
+	}
+
+	public void setTekija(String tekija) {
+		this.tekija = tekija;
+	}
+
+	public String getVuosi() {
+		return vuosi;
+	}
+
+	public void setVuosi(String vuosi) {
+		this.vuosi = vuosi;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getHinta() {
+		return hinta;
+	}
+
+	public void setHinta(String hinta) {
+		this.hinta = hinta;
+	}
+
+	@Override
+	public String toString() {
+		return "Kirja [nimi=" + nimi + ", tekija=" + tekija + ", vuosi=" + vuosi + ", isbn=" + isbn + ", hinta=" + hinta
+				+ "]";
+	}
+	
 }
--- Kirjakauppaprojekti/src/main/java/fi/hh/swd20/Kirjakauppaprojekti/web/KirjaController.java
+++ Kirjakauppaprojekti/src/main/java/fi/hh/swd20/Kirjakauppaprojekti/web/KirjaController.java
@@ -1,5 +1,14 @@
 package fi.hh.swd20.Kirjakauppaprojekti.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+@Controller
 public class KirjaController {
 
+	@RequestMapping(value="/index", method=RequestMethod.GET)
+	public String returnIndex(){
+		return "index";
+	}
+	
 }
