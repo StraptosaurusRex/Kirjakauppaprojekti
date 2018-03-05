335fc540ac77b6a6223cf539148fe95e1c66e93e
Sara Koivunen <you@example.com>
2018-03-05 03:40:00 +0200

    eight commit

---
+++ Kirjakauppaprojekti/src/main/java/fi/hh/swd20/Kirjakauppaprojekti/domain/UserRepository.java
@@ -0,0 +1,10 @@
+package fi.hh.swd20.Kirjakauppaprojekti.domain;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+	User findByUsername(String username);
+}
